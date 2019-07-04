2314dd9cf55648891c4d254d3489933c32f19719
CARLOS ANDRE <programadorfsaba@gmail.com>
2019-07-04 15:40:49 -0300

    commit

--- core/save.php
+++ core/save.php
@@ -640,7 +640,7 @@ switch ($value) {
 		}
 		$cad = crud::AlterarPedido($codControle, $statusPedido, $mensagemAlterar, $idInstituicao,$dataAlteracao,$dataFechamento);
 		if ($cad == true) {
-			echo 1;
+			echo 2;
 			if(!$email ==''){
 				crud::enviarEmailPedido($email,$subject,$nomeUsuario);
 			}
