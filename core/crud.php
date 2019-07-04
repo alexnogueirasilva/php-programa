23a22d2f221e6385a1ffcb69f82c98fcc1c4774d
CARLOS ANDRE <programadorfsaba@gmail.com>
2019-07-04 17:22:05 -0300

    commit

--- core/crud.php
+++ core/crud.php
@@ -1182,17 +1182,15 @@ class crud
 		
 				$subject = "Erro no sistema"; // assunto
 				$message = "Usuario: " .$nomeUsuario. " identificou o erro no sistema <br><br> " . "\r\n";
-				$message = "mensagem do usuario: " .$mensagem. "  <br><br> " . "\r\n";
-				$message = "erro ocorrido em: " .$data. "  <br><br> " . "\r\n";
+				$message .= "mensagem do usuario: " .$mensagem. "  <br><br> " . "\r\n";
+				$message .= "erro ocorrido em: " .$data. "  <br><br> " . "\r\n";
 				$message .= "<a href=http://sistemaocorrencia.devnogueira.online> Click aqui para acessar o sistema</a> <br><br><br> " . "\r\n";
 				$message .= "error ".$erro. " <br><br>"  . "\r\n";
 				$message .= "favor da tratamento" . "\r\n";
 				$headers = 'MIME-Version: 1.0' . "\r\n";
 				$headers .= 'content-type: text/html; charset=iso-8859-1' . "\r\n";
-				$headers .= 'From:< noreply@sistemadevnogueira.online>' . "\r\n"; //email de envio
-				//$headers .= 'CC:<vendas2@fabmed.com.br >' . "\r\n"; //email com copia
+				$headers .= 'From:< noreply@sistemadevnogueira.online>' . "\r\n"; //email de envio			
 				$headers .= 'CC:<'.$email.'>' . "\r\n"; //email com copia
-				//$headers .= 'CC:< '.$ email." ". "\r\n"; //email com copia
 				$headers .= 'Reply-To: <suporte@sistemadevnogueira.online>' . "\r\n"; //email para resposta
 
 				mail($to, $subject, $message, $headers);
