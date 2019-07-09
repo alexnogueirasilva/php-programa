3c786fd2f34d18f0e1cabf01378133221f2ba2b9
CARLOS ANDRE <programadorfsaba@gmail.com>
2019-07-09 16:46:24 -0300

    COMMIT

--- main/js/pedido.js
+++ main/js/pedido.js
@@ -176,7 +176,7 @@ $("#frmAlterarPedido").on('submit', (function (e) {
             $("#alteraPedido").prop("disabled", true);
         },
         success: function (data) {
-             alert(data);
+            // alert(data);
 
             if (data == 1) {
                 swal({
